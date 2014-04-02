9b432d72594a4a55360d695b03523d822d0973d1
Jonas Jelten <jj@sft.mx>
2014-04-02 06:09:02 +0200

    xtier: added qemu patches for enabling xtier support

--- include/exec/cpu-all.h
+++ include/exec/cpu-all.h
@@ -488,6 +488,8 @@ void qemu_mutex_lock_ramlist(void);
 void qemu_mutex_unlock_ramlist(void);
 #endif /* !CONFIG_USER_ONLY */
 
+hwaddr gva_to_hva(CPUState *state, hwaddr addr);
+
 int cpu_memory_rw_debug(CPUState *cpu, target_ulong addr,
                         uint8_t *buf, int len, int is_write);
 
